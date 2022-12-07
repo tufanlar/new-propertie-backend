df41cb7c9801a0b54c194291ce358744e10fa1e0
Mahmut TUFAN <mahmut.tufan789@gmail.com>
2022-12-07 12:11:25 +0300

    updated

---
+++ README.md
@@ -0,0 +1,1 @@
+# new-propertie-backend
--- index.js
+++ index.js
@@ -1,27 +1,8 @@
 const http = require('http');
 
-// Server uygulaması
 const app = require('./app');
 
 const server = http.createServer(app);
 
 module.exports = server
 
-/*
-var express = require('express');
-var app     = express();
-var server  = require('http').createServer(app);
-var io      = require('socket.io').listen(server);
-...
-server.listen(1234);
-However, app.listen() also returns the HTTP server instance, so with a bit of rewriting you can achieve something similar without creating an HTTP server yourself:
-
-var express   = require('express');
-var app       = express();
-// app.use/routes/etc...
-var server    = app.listen(3033);
-var io        = require('socket.io').listen(server);
-io.sockets.on('connection', function (socket) {
-  ...
-});
-*/
